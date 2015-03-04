3f6ae98f26baa4a526040c724a3ac2976355dab6
Bruno Lemos <brunohplemos@gmail.com>
2015-03-04 17:02:00 -0300

    First commit

---
+++ Slack/Slack.Shared/Views/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Slack.Common;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Slack.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public NavigationHelper NavigationHelper { get { return this.navigationHelper; } }
+        private NavigationHelper navigationHelper;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+        
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+        
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
